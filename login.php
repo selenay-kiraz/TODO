da9b2760d322db4c5e60c800ad19a15c58c8cd1e
Selenay <61632405+selenay-kiraz@users.noreply.github.com>
2020-10-09 02:55:44 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<!<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title> Login Page </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div id="frm">
+    <form action="index.php" method="post">
+        <p>
+            <label>Username:</label>
+            <input type="text" id="user" name="user" />
+        </p>
+        <p>
+            <label>Password:</label>
+            <input type="password" id="pass" name="pass" />
+        </p>
+        <p>
+            <input type="submit" id="btn" name="Login" />
+        </p>
+    </form>
+</div>
+</body>
+</html>
+</HTML>
